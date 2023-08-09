0df3eb2c88e2f6f059fc9b634dce13c1cd2aec16
Enes Sariyildiz <ens.sryldz@gmail.com>
2023-08-09 17:55:19 +0300

    Open closed prenciple

--- Solid.App/Program.cs
+++ Solid.App/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Solid.App.OCPBad;
+using static Solid.App.OCPBad.SalaryCalculator;
+
+SalaryCalculator salaryCalculator = new SalaryCalculator();
+
+
+Console.WriteLine($"Low salary : {salaryCalculator.Calculate(1000, SalaryType.Low)}");
+Console.WriteLine($"Middle salary : {salaryCalculator.Calculate(1000, SalaryType.Middle)}");
+Console.WriteLine($"High salary : {salaryCalculator.Calculate(1000, SalaryType.High)}");
+
+Console.ReadKey();
