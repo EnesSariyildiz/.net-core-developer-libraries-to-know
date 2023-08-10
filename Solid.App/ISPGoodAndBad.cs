467dcc4ebcf8c22166c55fcc2b60b3a8e1416275
Enes Sariyildiz <ens.sryldz@gmail.com>
2023-08-10 18:39:29 +0300

    Interface segregation prenciple

---
+++ Solid.App/ISPGoodAndBad.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.AppGoodAndBad
+{
+    //1. Class Library Read Impl
+    //1. Class Library Create/Update/Delete Impl
+
+
+    //1. Class Library
+    public class ReadProductRepository : IReadRepository
+    {
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //1. Class Library
+    public class WriteProductRepository : IWriteRepository
+    {
+        public Product Create(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Delete(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Update(Product p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    /*
+     * Bad Way
+    public interface IPRoductRepository
+    {
+        List<Product> GetList();
+        Product GetById(int id);
+
+        Product Create(Product p);
+
+        Product Update(Product p);
+
+        Product Delete(Product p);
+    }
+    */
+
+    public interface IReadRepository
+    {
+        List<Product> GetList();
+        Product GetById(int id);
+    }
+
+
+    public interface IWriteRepository
+    {
+        Product Create(Product p);
+
+        Product Update(Product p);
+
+        Product Delete(Product p);
+    }
+}
--- Solid.App/obj/Debug/net7.0/Solid.App.AssemblyInfo.cs
+++ Solid.App/obj/Debug/net7.0/Solid.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Solid.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9acd4904aec2997a36baffbb174a4d53861967")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Solid.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Solid.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
